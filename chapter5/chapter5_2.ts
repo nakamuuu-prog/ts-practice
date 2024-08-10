9f2e98cb0cfe5e10131f502ff4c2c8b4b1ae0e8d
nakamuuu-prog <affilimoney2@gmail.com>
2024-08-11 00:21:51 +0900

    ファイルを章ごとにまとめる

--- chapter5/chapter5_2.ts
+++ chapter5/chapter5_2.ts
@@ -1,3 +1,73 @@
+// オブジェクト型
+// 型Aと型Bがあるとき、次の両方の条件が満たされることで型Bが型Aのサブタイプとしてみなされる(B <: A)
+// ・型Aに存在するすべてのプロパティが型Bにも存在する
+// ・型Bの各プロパティの型が、対応する型Aのプロパティの型のサブタイプである
+
+const chapter5_2_1 = () => {
+  interface Person {
+    name: string;
+    age: number;
+  }
+  // 型A
+  let person: Person;
+
+  let john = {
+    name: "John",
+    age: 30,
+    gender: "male",
+  };
+
+  // 型B
+  // johnはPerson型にはないgenderプロパティを持っているが、Person型にあるnameとage両方のプロパティを持っているためサブタイプとして認識される
+  person = john;
+
+  // 型B
+  let jane = {
+    name: "John",
+    age: "25",
+  };
+
+  // janeはPerson型にあるnameとage両方のプロパティを持っているが、ageがstring型のため、サブタイプとして認識されない
+  // 型 '{ name: string; age: string; }' を型 'Person' に割り当てることはできません。
+  // プロパティ 'age' の型に互換性がありません。
+  //   型 'string' を型 'number' に割り当てることはできません。ts(2322)
+  // person = jane;
+
+  // 型B
+  let alice = {
+    name: "Alice",
+  };
+
+  // aliceはageプロパティが存在しないのでサブタイプとして認識されない
+  // Person型のageがないのでエラー
+  // プロパティ 'age' は型 '{ name: string; }' にありませんが、型 'Person' では必須です。ts(2741)
+  // person = alice;
+};
+
+// たぶんjaneはageがnumberとstring型のUnion型だったらサブタイプとして認識されるはず
+const chapter5_2_1_test = () => {
+  interface Person {
+    name: string;
+    age: number | string;
+  }
+  let person: Person;
+
+  let john = {
+    name: "John",
+    age: 30,
+    gender: "male",
+  };
+
+  person = john;
+
+  let jane = {
+    name: "John",
+    age: "25",
+  };
+
+  person = jane;
+};
+
 // 関数型
 // 戻り値の型
 // 関数Aと関数Bがあるとき、「Bの戻り値の型 <: Aの戻り値の型」の条件が満たされることで関数Bが関数Aのサブタイプとしてみなされる
--- chapter5/chapter5_4.ts
+++ chapter5/chapter5_4.ts
@@ -1,3 +1,22 @@
+const chapter5_4_1 = () => {
+  // string | number型
+  let x = Math.random() > 0.5 ? 1 : "Hello, TypeScript";
+
+  // xがnumber型の可能性があるのでエラー
+  // プロパティ 'toUpperCase' は型 'string | number' に存在しません。
+  // プロパティ 'toUpperCase' は型 'number' に存在しません。ts(2339)
+  // x.toUpperCase();
+
+  // 直接string型を代入するとstring型で型推論される
+  x = "narrowing"; // string型
+  // string型に絞り込まれているのでstring型の関数を呼べる
+  x.toUpperCase();
+
+  // number型にしても同じことが起こる
+  x = 123;
+  x.toFixed();
+};
+
 // 型ガード
 // 等価演算子を使って絞り込む
 const chapter5_4_2_1 = () => {
@@ -150,4 +169,80 @@ const chapter5_4_2_5 = () => {
         console.log(`Area: ${shape.width ** 2}`);
     }
   }
+};
+
+const chapter5_4_3 = () => {
+  const scenario1 = () => {
+    // colorというオブジェクトを定義
+    // colorのプロパティは数値の配列か、カラーコードで指定できるようにしたい
+    const color = {
+      red: [255, 0, 0],
+      green: "#00ff00",
+      bleu: [0, 0, 255], // blueがタイプミスしてしまっている
+    };
+
+    // colorは以下のように型推論される
+    // const color: {
+    //   red: number[];
+    //   green: string;
+    //   bleu: number[];
+    // };
+
+    // greenだけはstring型なので、string型の関数が呼べる
+    const greenNormalized = color.green.toUpperCase();
+  };
+  const scenario2 = () => {
+    // scenario1でのタイプミスを防ぐために型を定義
+    type RGB = [red: number, green: number, blue: number]; //　ラベル付きTuple型
+
+    // Colorは数値の配列かカラーコードを表すためのstring型を代入できるようにしたいので、各プロパティをRGBのTuple型とstring型にする
+    interface Color {
+      red: RGB | string;
+      green: RGB | string;
+      blue: RGB | string;
+    }
+
+    const color: Color = {
+      red: [255, 0, 0],
+      green: "#00ff00",
+      blue: [0, 0, 255],
+      // 型を宣言することででタイプミスに気づけるよになる
+      // オブジェクト リテラルは既知のプロパティのみ指定できます。'bleu' は型 'Color' に存在しません。ts(2353)
+      // bleu: [0, 0, 255],
+    };
+
+    // scenario1ではgreenがstring型で型推論されていたため、string型の関数が呼び出せていたが、明確に型を定義したためRGB | string型になってしまいエラーになる
+    // プロパティ 'toUpperCase' は型 'string | RGB' に存在しません。
+    // プロパティ 'toUpperCase' は型 'RGB' に存在しません。ts(2339)
+    // const greenNormalized = color.green.toUpperCase();
+  };
+
+  const scenario3 = () => {
+    type RGB = [red: number, green: number, blue: number]; //　ラベル付きTuple型
+
+    interface Color {
+      red: RGB | string;
+      green: RGB | string;
+      blue: RGB | string;
+    }
+
+    // scenario2のようなエラーを防ぐためにsatisfiesを使うことで、型推論の結果を維持しながら型の一致を検証できる
+    const color = {
+      red: [255, 0, 0],
+      green: "#00ff00",
+      blue: [0, 0, 255],
+      // オブジェクト リテラルは既知のプロパティのみ指定できます。'bleu' は型 'Color' に存在しません。ts(2353)
+      // bleu: [0, 0, 255],
+    } satisfies Color;
+
+    // colorは以下のように型推論される
+    // const color: {
+    //   red: [number, number, number];
+    //   green: string;
+    //   blue: [number, number, number];
+    // };
+
+    // 型を定義していてもstring型として型推論されているため、string型の関数が呼べる
+    const greenNormalized = color.green.toUpperCase();
+  };
 };
